7a8a86714e77481b210af88791b00648497629d5
Víctor M <victormunozfer@gmail.com>
2022-11-08 22:42:32 -0300

    Incorporacion de platos

--- src/screens/Products/MenuScreen.js
+++ src/screens/Products/MenuScreen.js
@@ -1,15 +1,33 @@
-import { View, Text,StyleSheet, ImageBackground } from 'react-native'
+import { View, Text,StyleSheet, ImageBackground,Image } from 'react-native'
 import React,{useState} from 'react'
 import { ScrollView, TextInput } from 'react-native-gesture-handler'
 import CustomInput from '../../components/CustomInput/CustomInput'
 import Entypo from 'react-native-vector-icons/Entypo';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
+
 import burgerCategory from '../../../Categorias/burger-categorie.jpeg';
 import saladCategory from '../../../Categorias/ensalada-categorie.jpeg';
 import pizzaCategory from '../../../Categorias/pizza-categorie.jpeg';
 import sushiCategory from '../../../Categorias/sushi-categorie.jpeg';
-import Colors from '../../../const/Colors';
-import { color } from '@rneui/base';
+
+import pizzaCarne from '../../../Comidas/pizza-carne.png';
+import pizzaQueso from '../../../Comidas/pizza-queso.png';
+import sushi1 from '../../../Comidas/sushi-salmon.png';
+import sushi2 from '../../../Comidas/sushi2.png';
+import sushi3 from '../../../Comidas/cocacola.png';
+import papasFritas from '../../../Comidas/papasfritas.png';
+import arosCebolla from '../../../Comidas/arosCebolla.png';
+import burger1 from '../../../Comidas/Hamburguesa-tocino-carne.png';
+import burger2 from '../../../Comidas/burger2.png';
+import burger3 from '../../../Comidas/burger3.png';
+import ensalada1 from '../../../Comidas/ensalada.png';
+import ensalada2 from '../../../Comidas/ensalada2.png';
+import ensalada3 from '../../../Comidas/ensalada3.png';
+import cocacola from '../../../Comidas/cocacola.png';
+import aguamineralGas from '../../../Comidas/aguamineralGAS.png';
+
+
+
 
 const MenuScreen = () => {
     const {product,setProduct} = useState('');
@@ -60,7 +78,80 @@ const MenuScreen = () => {
      </ImageBackground>
         
     </ScrollView>
-    <Text style={styles.title}>Platos</Text>
+
+    <Text style={[styles.title,{marginBottom:20}]}>Pizzas</Text>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+         <View style={{marginLeft:20}}>
+         <Image style={styles.productImage} source={pizzaCarne}/>
+         </View>
+         <View style={{marginLeft:20}}>
+         <Image style={styles.productImage} source={pizzaQueso}/>
+         </View>
+    </ScrollView>
+
+   <Text style={[styles.title,{marginBottom:20}]}>Sushi</Text>
+   <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+   <View style={{marginLeft:20}}>
+   <Image style={styles.productImage} source={sushi1}/>
+   </View>
+   <View style={{marginLeft:20}}>
+   <Image style={styles.productImage} source={sushi2}/>
+   </View>
+   <View style={{marginLeft:20}}>
+   <Image style={styles.productImage} source={sushi3}/>
+   </View>
+   </ScrollView>
+
+    <Text style={[styles.title,{marginBottom:20}]}>Hamburguesas</Text>
+   <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+     <View style={{marginLeft:20}}>
+     <Image style={styles.productImage} source={burger1}/>
+     </View>
+     <View style={{marginLeft:20}}>
+     <Image style={styles.productImage} source={burger2}/>
+     </View>
+     <View style={{marginLeft:20}}>
+     <Image style={styles.productImage} source={burger3}/>
+     </View>
+   </ScrollView>
+   
+   <Text style={[styles.title,{marginBottom:20}]}>Ensaladas</Text>
+   <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+     <View style={{marginLeft:20}}>
+     <Image style={styles.productImage} source={ensalada1}/>
+     </View>
+     <View style={{marginLeft:20}}>
+     <Image style={styles.productImage} source={ensalada2}/>
+     </View>
+     <View style={{marginLeft:20}}>
+     <Image style={styles.productImage} source={ensalada3}/>
+     </View>
+   </ScrollView>
+
+   <Text style={[styles.title,{marginBottom:20}]}>Acompañantes</Text>
+   <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+     <View style={{marginLeft:20}}>
+     <Image style={styles.productImage} source={papasFritas}/>
+     </View>
+     <View style={{marginLeft:20}}>
+     <Image style={styles.productImage} source={arosCebolla}/>
+     </View>
+   </ScrollView>
+
+   <Text style={[styles.title,{marginBottom:20}]}>Bebestibles</Text>
+   <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+     <View style={{marginLeft:20}}>
+     <Image style={styles.productImage} source={cocacola}/>
+     </View>
+     <View style={{marginLeft:20}}>
+     <Image style={styles.productImage} source={aguamineralGas}/>
+     </View>
+   </ScrollView>
+
+
+
+
+    
     
     </ScrollView>
 
@@ -89,8 +180,8 @@ const styles = StyleSheet.create({
 
     },
     productImage:{
-        height: 320,
-        width: 260,
+        height:180,
+        width: 180,
         borderRadius: 10,
        marginBottom: 12.0,
        marginTop: 12.0,
